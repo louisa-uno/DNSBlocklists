4e0eee3cee698b2cac4c437545774752dc239ff5
Luois45 <github@louis45.de>
2022-02-02 16:26:33 +0100

    Changed structure of lists.json

--- fetch-lists.py
+++ fetch-lists.py
@@ -5,7 +5,7 @@ import shutil
 import requests
 # from github import Github, InputGitAuthor
 
-config = json.load(open("config.json"))
+# config = json.load(open("config.json"))
 lists = json.load(open("lists.json"))
 
 for f in os.listdir():
@@ -15,19 +15,30 @@ for f in os.listdir():
 		shutil.rmtree(f)
 
 list_file = ""
-for category in lists:
-	category_file = ""
-	os.mkdir(category)
-	for list in lists[category]:
-		response = requests.get(lists[category][list]["url"])
-		with open(category + "/" + list + ".txt", "w", encoding="utf-8") as f:
-			f.write(response.text)
-		category_file += response.text + "\n"
+for list in range(0, len(lists)):
+	try:
+		os.mkdir(lists[list]["category"])
+	except FileExistsError:
+		pass
+	response = requests.get(lists[list]["url"])
+	with open(lists[list]["category"] + "/" + lists[list]["name"] + ".txt",
+	          "w",
+	          encoding="utf-8") as f:
+		f.write(response.text)
+
+full_list = ""
+for category in os.listdir():
+	if category == ".git" or category == ".github" or "." in category:
+		continue
+	category_list = ""
+	for list in os.listdir(category):
+		with open(category + "/" + list, "r", encoding="utf-8") as f:
+			category_list += f.read() + "\n"
 	with open(category + ".txt", "w", encoding="utf-8") as f:
-		f.write(category_file)
-	list_file += category_file + "\n"
+		f.write(category_list)
+	full_list += category_list
 with open("list" + ".txt", "w", encoding="utf-8") as f:
-	f.write(list_file)
+	f.write(full_list)
 
 # def push(path, message, content, branch):
 # 	author = InputGitAuthor("Luois45 - Update lists", "github@louis45.de")
