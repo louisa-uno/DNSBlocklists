e55bc5671829daaec82bbaf6368fdceb60e957b5
Luois45 <github@louis45.de>
2022-02-24 02:19:10 +0100

    Improved the Pi-hole blocklist

--- convert-lists.py
+++ convert-lists.py
@@ -15,16 +15,17 @@ def convertList(path):
 			commentSubstrings = ["# ", " #"]
 			if any(x in line for x in commentSubstrings):
 				line = line.split("#")[0].replace(" ", "") + "\n"
-				if line == "\n":
-					continue
 			if line == "\n":
 				continue
 			line = line.replace("0.0.0.0 ", "").replace("127.0.0.1 ", "")
-			adguardOut += line
-			if not "!+ " in line:
-				piholeOut += line
-	with open(path.replace(".txt", ".adguard"), "w",
-	          encoding="utf-8") as f:
+			adguardLine = line
+			adguardOut += adguardLine
+			if "!+ " not in line:
+				piholeLine = line
+				piholeLine = piholeLine.split("^")[0]
+				piholeLine = piholeLine.replace("||", "")
+				piholeOut += piholeLine
+	with open(path.replace(".txt", ".adguard"), "w", encoding="utf-8") as f:
 		f.write(adguardOut)
 	with open(path.replace(".txt", ".pihole"), "w", encoding="utf-8") as f:
 		f.write(piholeOut)
