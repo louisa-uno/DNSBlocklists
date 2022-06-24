b07479e4323790dac0904caefe64ea9062726c7b
Luois45 <github@louis45.de>
2022-06-24 22:22:36 +0200

    Fixed bugs

--- convert-lists.py
+++ convert-lists.py
@@ -6,35 +6,51 @@ with open("allowlist.txt", "r", encoding="utf-8") as f:
 		ALLOWLIST.append(line.replace("\n", ""))
 
 
-def convertList(path):
+def checkIfValidFile(path):
 	if not ".txt" in path:
-		return
+		return False
 	if "allowlist.txt" in path:
-		return
+		return False
 	if ".adguard" in path or ".pihole" in path:
+		return False
+	return True
+
+
+def convertLine(line):
+	if any(x in line for x in ALLOWLIST):
+		return ""
+	line = line.replace("0.0.0.0 ", "").replace("127.0.0.1 ", "")
+	commentSubstrings = ["# ", " #"]
+	if any(x in line for x in commentSubstrings):
+		line = line.split("#")[0].replace(" ", "") + "\n"
+	return line
+
+
+def convertAdguardLine(line):
+	return line
+
+
+def convertPiholeLine(line):
+	if any(x in line for x in ["/", "*", "$", "!", "<", ">"]):
+		return ""
+	line = line.replace("||", "")
+	if "^" in line:
+		line = line.split("^")[0] + "\n"
+	if "#[" in line:
+		line = line.replace("#[", "").replace("]", "")
+	return line
+
+
+def convertList(path):
+	if checkIfValidFile(path) is False:
 		return
 	with open(path, "r", encoding="utf-8") as f:
 		filteredOut, adguardOut, piholeOut = "", "", ""
 		for line in f.readlines():
-			forbiddenSubstrings = ["<", "! ", "  ", "!--"]
-			if any(x in line for x in forbiddenSubstrings):
-				continue
-			if any(x in line for x in ALLOWLIST):
-				continue
-			commentSubstrings = ["# ", " #"]
-			if any(x in line for x in commentSubstrings):
-				line = line.split("#")[0].replace(" ", "") + "\n"
-			if line == "\n":
-				continue
-			line = line.replace("0.0.0.0 ", "").replace("127.0.0.1 ", "")
+			line = convertLine(line)
 			filteredOut += line
-			adguardLine = line
-			adguardOut += adguardLine
-			if "!+ " not in line:
-				piholeLine = line
-				piholeLine = piholeLine.split("^")[0]
-				piholeLine = piholeLine.replace("||", "")
-				piholeOut += piholeLine
+			adguardOut += convertAdguardLine(line)
+			piholeOut += convertPiholeLine(line)
 	with open(path, "w", encoding="utf-8") as f:
 		f.write(filteredOut)
 	with open(path.replace(".txt", ".adguard"), "w", encoding="utf-8") as f:
